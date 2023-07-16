29ab7899d2a56a321f92dc85ca8d53dc34d0ab41
Seongtae <stae1102@gmail.com>
2023-07-16 14:58:56 +0900

    feat: 기본 class 추가

---
+++ class-decorator.ts
@@ -0,0 +1,15 @@
+// 1. class
+class A {
+  // 2. property
+  b: string = 'Hello';
+
+  // 3. accessor
+  get c(): string {
+    return `${this.b} World!`;
+  }
+
+  // 4. method
+  d(/* 5. parameter */ e: string): void {
+    console.log(e);
+  }
+}
